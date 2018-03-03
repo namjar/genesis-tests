47f9ffd5b2ba477ac0ff774c42a90b180e05a9a1
arkuz <arkuzka@mail.ru>
2018-03-03 18:44:16 +0300

    changed tablesDict on tablesList

--- quickStarterMonitoring.py
+++ quickStarterMonitoring.py
@@ -34,25 +34,8 @@ dbParams = {
 logAll = int(args.logAll)
 nodesCount = int(args.nodesCount)
 timeout = int(args.timeout)
+tablesList = []             # list of table names
 
-tablesDict = {          # for code on 162 string
-    0:['countTables'],
-    1:['blocks'],
-    2:['contracts'],
-    3:['history'],
-    4:['keys'],
-    5:['languages'],
-    6:['members'],
-    7:['menus'],
-    8:['notifications'],
-    9:['pages'],
-    10:['parameters'],
-    11:['roles_assign'],
-    12:['roles_list'],
-    13:['sections'],
-    14:['signatures'],
-    15:['tables']
-}
 
 def getCountDBObjects(dbParams):
     connect = psycopg2.connect(host=dbParams['dbHost'],
